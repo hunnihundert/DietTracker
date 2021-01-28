e86f281d2903e1bb6deee5022fd6289f2b527b35
Hoon Jung <hoon.michael.jung@gmail.com>
2021-01-29 07:28:46 +0900

    Change minor things in starting foundation

--- app/src/main/java/com/hooni/diettracker/MainActivity.kt
+++ app/src/main/java/com/hooni/diettracker/MainActivity.kt
@@ -2,10 +2,21 @@ package com.hooni.diettracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController,appBarConfiguration)
     }
 }
